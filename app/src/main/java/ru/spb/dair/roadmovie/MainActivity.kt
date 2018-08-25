a566357075a5f80adaf23e0f06c668c0cd68bb92
Lebedev-Schmidthof Vladimir <dair@joybits.org>
2018-08-25 23:03:19 +0300

    Initial commit: one activity, one service

---
+++ app/src/main/java/ru/spb/dair/roadmovie/MainActivity.kt
@@ -0,0 +1,56 @@
+package ru.spb.dair.roadmovie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Context.ACTIVITY_SERVICE
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var _mainButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        _mainButton = this.findViewById<Button>(R.id.main_button)
+        _mainButton.setOnClickListener {
+            onButtonClick()
+        }
+    }
+
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (isMyServiceRunning(MyService::class.java)) {
+            _mainButton.text = "Остановить поездку"
+        }
+        else {
+            _mainButton.text = "Начать поездку"
+        }
+    }
+
+    private fun onButtonClick() {
+        val intent = Intent(this, MyService::class.java)
+        if (!isMyServiceRunning(MyService::class.java)) {
+            startService(intent)
+        }
+        else {
+            stopService(intent)
+        }
+    }
+}
