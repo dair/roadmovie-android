67e776e4690c5f751d460e2f0c2b1286ee4d8075
Lebedev-Schmidthof Vladimir <dair@joybits.org>
2018-08-30 11:39:56 +0300

    Trying to handle locations

--- app/src/main/java/ru/spb/dair/roadmovie/MainActivity.kt
+++ app/src/main/java/ru/spb/dair/roadmovie/MainActivity.kt
@@ -7,11 +7,67 @@ import android.content.Context.ACTIVITY_SERVICE
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import android.widget.TextView
+import android.widget.Toast
+import android.content.ComponentName
+import ru.spb.dair.roadmovie.MyService.LocalBinder
+import android.os.IBinder
+import android.content.ServiceConnection
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var _mainButton: Button
+    lateinit var _unloadLabel: TextView
+
+    private var mBoundService: MyService? = null
+    private var mShouldUnbind: Boolean = false
+
+    private lateinit var timer: Timer
+
+    private val mConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            mBoundService = (service as MyService.LocalBinder).service
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+            mBoundService = null
+        }
+    }
+
+    fun doBindService() {
+        // Attempts to establish a connection with the service.  We use an
+        // explicit class name because we want a specific service
+        // implementation that we know will be running in our own process
+        // (and thus won't be supporting component replacement by other
+        // applications).
+        if (bindService(Intent(this@MainActivity, MyService::class.java),
+                        mConnection, Context.BIND_AUTO_CREATE)) {
+            mShouldUnbind = true
+        }
+    }
+
+    fun doUnbindService() {
+        if (mShouldUnbind) {
+            // Release information about the service's state.
+            unbindService(mConnection)
+            mShouldUnbind = false
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +77,9 @@ class MainActivity : AppCompatActivity() {
         _mainButton.setOnClickListener {
             onButtonClick()
         }
+
+        _unloadLabel = this.findViewById(R.id.unloadLabel)
+        _unloadLabel.visibility = INVISIBLE
     }
 
     private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
@@ -36,15 +95,44 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        if (isMyServiceRunning(MyService::class.java)) {
+            doBindService()
+        }
+
         updateButtonText()
+
+        timer = Timer("updateTimer")
+        timer.scheduleAtFixedRate(1000, 1000, {
+            runOnUiThread {
+                updateButtonText()
+            }
+        })
+    }
+
+    override fun onPause() {
+        timer.cancel()
+        if (isMyServiceRunning(MyService::class.java)) {
+            doUnbindService()
+        }
+        super.onPause()
     }
 
     private fun updateButtonText() {
         if (isMyServiceRunning(MyService::class.java)) {
             _mainButton.text = "Остановить поездку"
+            if (mBoundService != null) {
+                val unload = mBoundService!!.userNotified
+                if (unload) {
+                    _unloadLabel.visibility = VISIBLE
+                }
+                else {
+                    _unloadLabel.visibility = INVISIBLE
+                }
+            }
         }
         else {
             _mainButton.text = "Начать поездку"
+            _unloadLabel.visibility = INVISIBLE
         }
     }
 
@@ -52,8 +140,11 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MyService::class.java)
         if (!isMyServiceRunning(MyService::class.java)) {
             startService(intent)
+            doBindService()
         }
         else {
+            mBoundService?.userNotified = false
+            doUnbindService()
             stopService(intent)
         }
 
--- app/src/main/java/ru/spb/dair/roadmovie/MyService.kt
+++ app/src/main/java/ru/spb/dair/roadmovie/MyService.kt
@@ -1,16 +1,136 @@
 package ru.spb.dair.roadmovie
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.support.v4.app.NotificationCompat
+import android.app.PendingIntent
+import android.support.v4.app.NotificationManagerCompat
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.location.Criteria.ACCURACY_HIGH
+import android.location.LocationManager.GPS_PROVIDER
+import android.media.RingtoneManager
+import android.os.*
+import android.support.v4.app.NotificationCompat.CATEGORY_ALARM
+import ru.spb.dair.roadmovie.MyService.LocalBinder
 import android.os.IBinder
 
-class MyService : Service() {
+
+
+
+class MyService : Service(), LocationListener {
+
+    val MIN_SPEED = 20 / 3.6
+    val MAX_TIME = 10 * 60 * 1000
+
+    var _locationManager: LocationManager? = null
+    var _lastLocationTime: Long = 0
+    var _totalTimePassed: Long = 0
+    public var userNotified = false
+    var _notificationId: Int = 0
+
+    inner class LocalBinder : Binder() {
+        internal val service: MyService
+            get() = this@MyService
+    }
+
+    private val mBinder = LocalBinder()
+
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        _lastLocationTime = 0
+        userNotified = false
+
+        createNotificationChannel()
+
+        _locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (_locationManager != null) {
+            _locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 0.0, this)
+        }
+
+//        Handler().postDelayed({
+//            notifyTheUser()
+//        }, 2000)
+
         return START_STICKY
     }
+
+    override fun onLocationChanged(location: Location) {
+        // Called when a new location is found by the network location provider.
+
+        if (location.speed < MIN_SPEED) {
+            return
+        }
+
+        if (_lastLocationTime == 0L) {
+            _lastLocationTime = location.time
+            _totalTimePassed = 0L
+            userNotified = false
+        }
+        else {
+            val delta = location.time - _lastLocationTime
+            _totalTimePassed += delta
+
+            if (!userNotified && _totalTimePassed > MAX_TIME) {
+                notifyTheUser()
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "RoadMovie notification channel"
+            val description = "To notify the user about timout"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("ru.spb.dair.RoadMovie", name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+    }
+
+
+    private fun notifyTheUser() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+
+        val mBuilder = NotificationCompat.Builder(this, "ru.spb.dair.RoadMovie")
+                .setContentTitle("Road Movie")
+                .setContentText("Пассажира пора высаживать")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setAutoCancel(false)
+                .setCategory(CATEGORY_ALARM)
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(_notificationId++, mBuilder.build())
+
+        userNotified = true
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+
+    override fun onProviderEnabled(provider: String) {}
+
+    override fun onProviderDisabled(provider: String) {}
+
 }
